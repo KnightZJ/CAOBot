e21bc8b6112eb00c3e0d426548eb2dabb89420fe
KnightZJ <2237754547@qq.com>
2022-07-26 16:55:56 +0800

    ✨ feat(module): processGroupMsg
    processGroupMsg function and fix last ci error

--- src/module.cpp
+++ src/module.cpp
@@ -1,6 +1,6 @@
 #include "module.h"
 
-const string map[] = {
+const string monthTable[] = {
   "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
 };
 
@@ -8,7 +8,7 @@ Module::Module(string _prefix, string _version, string date, string time, string
   updateTime = date.substr(7, 4) + "/";
   string sub = date.substr(0, 3);
   for (int i = 0; i < 12; ++i)
-    if (map[i] == sub) {
+    if (monthTable[i] == sub) {
       updateTime += to_string(i+1) + "/";
     }
   updateTime += date.substr(4, 2) + " " + time.substr(0, 5);
@@ -22,3 +22,4 @@ string Module::help() {
         +"构建时间：" + updateTime + "\n"
         +"模块功能：" + desc;
 }
+
--- src/module.h
+++ src/module.h
@@ -2,12 +2,17 @@
 #define __MODULE_H__
 
 #include <string>
+#include <vector>
+#include "utils.h"
 
 using namespace std;
+using namespace MiraiCP;
 
 class Module {
 public:
   Module(string _prefix, string _version, string date, string time, string _desc);
+  bool processGroupMessage(ParsedMessage& pm);
+protected:
   string help();
 protected:
   string version;
